f2600cd3ca89d41cc66226663b1ac21ed510f2ab
Ikako13 <venuswil76@gmail.com>
2024-04-21 23:02:20 +0400

    second commit

---
+++ day 0/lesson_2.py
@@ -0,0 +1,22 @@
+name="ikako"
+
+
+lastname="suladze"
+
+
+height=1.57
+
+
+age=13
+knows_programing= True
+is_ugly= False
+
+print ("ikako "+" suladze")
+
+print ("ikako "+" 13")
+
+print (type ("ikako"))
+print (type ("suladze"))
+print (type (13))
+print (type (1.57))
+print (type (knows_programing))
