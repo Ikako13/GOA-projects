f20b1597416a462efaf80ee6caf2070f186e835d
Ikako13 <venuswil76@gmail.com>
2024-06-13 18:53:15 +0400

    fifteenth commit

--- day 14/homework/lesson14.py
+++ day 14/homework/lesson14.py
@@ -1,35 +1,29 @@
 
-# for q in range(0,30):
-#     print(q)
+for q in range(0,30):
+    print(q)
 
-# for i in range(0,20):
-#     if i % 2 == 0:
-#         print(i)
+for i in range(0,20):
+    if i % 2 == 0:
+        print(i)
 
-# for x in range(14):
-#     if x == 14: break
-#     print(x)
-# else:
-#     print("its finished! ")
+for x in range(14):
+    if x == 14: break
+    print(x)
+else:
+    print("its finished! ")
 
-# for XX in range(25):
-#     if XX == 12: continue
-#     print(XX)
-# else:
-#     print("it's finished")    
+for XX in range(25):
+    if XX == 12: continue
+    print(XX)
+else:
+    print("it's finished")    
 
-# for IX in range (0,100):
-#     if IX % 3 == 0:
-#         print(IX)
+for IX in range (0,100):
+    if IX % 3 == 0:
+        print(IX)
 
-# Num = (input("do you like goa: "))
-# if Num == "yes":
-#     print("you are chad")
-# elif Num == "no":
-#     print("You are not chad")
-
-i = 1
-while i < 6:
-    print(i)
-    if i == 3:
-        break
+Num = (input("do you like goa: "))
+if Num == "yes":
+    print("you are chad")
+elif Num == "no":
+    print("You are not chad")
