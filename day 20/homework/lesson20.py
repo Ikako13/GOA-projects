498d66ff49780522cd1035ae333065bae735ab56
Ikako13 <venuswil76@gmail.com>
2024-07-08 19:59:03 +0400

    tventyth commit

---
+++ day 20/homework/lesson20.py
@@ -0,0 +1,20 @@
+def hello(academy):
+    print(academy)
+hello("goa is best")
+
+def sum(a, b):
+    result = (a + b)
+    print(result)    
+sum(4, 5)
+
+def to_string(value):
+    print(str(value))
+to_string(15)
+
+def print_type(hi):
+    print(type(hi))
+print_type(True)  
+
+def to_int(value):
+    print(int(value))
+to_int("27")    
---
+++ day 22/homework/lesson21.py
@@ -0,0 +1,35 @@
+#def  my_function(X):
+    #return 5 + X
+#print(my_function(5))
+
+def my_function(x):
+    return 2 + x
+print(my_function(2))
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
